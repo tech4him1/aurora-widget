e48b38546a9cfc6d58f92a70594a9cba38f7a087
Caleb <inchristalway-tech4him@yahoo.com>
2017-03-04 18:57:14 -0700

    Use window object and better throttle method for window resizing.

--- widget.js
+++ widget.js
@@ -1,8 +1,27 @@
-const throttle = require('throttleit');
-const win = nw.Window.get();
+(function() {
+    var throttle = function(type, name, obj) {
+        obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+             requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+
+    /* init - you can init any event */
+    throttle("resize", "optimizedResize");
+})();
+
 /* Keep window width and height equal. */
-win.on('resize', throttle((width, height)=>{
-  if (width !== height) {
-    win.width = height;
+window.addEventListener('optimizedResize', (e) => {
+  const width = e.currentTarget.outerWidth;
+  const height = e.currentTarget.outerHeight;
+  if (height != width) {
+    window.resizeTo(width, width);
   }
-}, 150));
+});
