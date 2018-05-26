15310fa3ff450baeadbded10c9e0ad99c4185d12
Caleb <inchristalway-tech4him@yahoo.com>
2018-05-26 12:56:57 -0600

    cleanup

--- widget.js
+++ widget.js
@@ -1,27 +1,26 @@
-(function() {
-    var throttle = function(type, name, obj) {
-        obj = obj || window;
-        var running = false;
-        var func = function() {
-            if (running) { return; }
-            running = true;
-             requestAnimationFrame(function() {
-                obj.dispatchEvent(new CustomEvent(name));
-                running = false;
-            });
-        };
-        obj.addEventListener(type, func);
-    };
+'use strict';
 
-    /* init - you can init any event */
-    throttle("resize", "optimizedResize");
-})();
+function throttle(type, name, target) {
+    var running = false;
+    var func = function() {
+        if (running) { return; }
+        running = true;
+        requestAnimationFrame(function() {
+            target.dispatchEvent(new CustomEvent(name));
+            running = false;
+        });
+    };
+    target.addEventListener(type, func);
+};
+
+/* init - you can init any event */
+throttle("resize", "optimizedResize", window);
 
 /* Keep window width and height equal. */
 window.addEventListener('optimizedResize', (e) => {
-  const width = e.currentTarget.outerWidth;
-  const height = e.currentTarget.outerHeight;
-  if (height != width) {
-    window.resizeTo(width, width);
-  }
+    const width = window.outerWidth;
+    const height = window.outerHeight;
+    if (height != width) {
+        window.resizeTo(width, width);
+    }
 });
